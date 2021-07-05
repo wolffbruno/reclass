e50129560e09343d9272996c618573e2515dbe0d
Bruno Wolff <bruno.wolff@clinicorp.com>
2021-07-04 22:31:59 -0300

    Composition pairs.

--- src/App.tsx
+++ src/App.tsx
@@ -64,6 +64,7 @@ class Relation {
 }
 
 function useForceUpdate(){
+    // @ts-ignore
     const [value, setValue] = useState(0); // integer state
     return () => setValue(value => value + 1); // update the state to force render
 }
